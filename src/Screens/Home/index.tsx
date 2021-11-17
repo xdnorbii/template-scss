4e6f8e52bf8541d8b52eea66d05b9f321b6ee6cd
Norbi <norbi.postol@nomorecoffeeplease.com>
2021-11-17 15:49:48 +0200

    init

---
+++ src/Screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+const Home = ()=>{
+    return (
+        <div className={styles.container}></div>
+    )
+}
+
+export default Home;
